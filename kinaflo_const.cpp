a96706bc230ccff4d001b2a71038dfdd64301ea1
kinaflo <79751028+kinaflo@users.noreply.github.com>
2021-10-18 22:52:30 +0700

    Create kinaflo_const.cpp
    constanta

---
+++ kinaflo_const.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  const string NAMA = "Yanuar Eka Susila";
+  const char JENIS_KELAMIN = 'L';
+  const double IPK = 3.99;
+  const int NIM = 211;
+
+ 
+  cout << "Nama : " << NAMA;
+  cout << "\n";
+ 
+  cout << "Jenis Kelamin : " << JENIS_KELAMIN;
+  cout << "\n";
+
+  cout << "Semoga dapat IPK : " << IPK;
+  cout << "\n";
+
+  cout << "NIM : " << NIM;
+  cout << "\n";
+
+  
+  return 0;
+}
