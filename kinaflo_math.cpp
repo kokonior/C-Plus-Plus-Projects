bff3371eb33a32498dee9c3f12aef87b0f601989
kinaflo <79751028+kinaflo@users.noreply.github.com>
2021-10-18 23:15:43 +0700

    Create kinaflo_math.cpp
    library function <math>

---
+++ kinaflo_math.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int W,X,Y,Z,ZZ ;
+    cout << "====================" << "\n";
+    cout << " masukan angka : " ;
+    cin >> W ;
+    cout << " masukan angka : " ;
+    cin >> X ;
+    cout << " masukan angka : " ;
+    cin >> Y ;
+    cout << " masukan angka : " ;
+    cin >> Z ;
+    cout << "====================" << "\n";
+
+    ZZ = W + X * Y - Z ;
+    cout << "Hasilnya : " << ZZ << " Rupiah" ;
+
+    return 0;
+}
