583481fc0d173cc0109ebede05c291b38524af0a
Handy A. Putra <48901714+handyaputra@users.noreply.github.com>
2021-10-19 18:18:55 +0700

    Update figure-calculator.cpp

--- figure-calculator.cpp
+++ figure-calculator.cpp
@@ -12,16 +12,16 @@ int main(){
   cout << "Insert your choice below : ";
   cin >> opt;
 
+  float area = 0, circumference = 0;
+
   if(opt == 1){
     float r,pi = 3.14;
 
     cout << "\n=== CIRCLE ===\n radius : ";
     cin >> r;
 
-    float area = pi*r*r;
-    float circumference = 2*pi*r;
-
-    cout << "\n=== Result ===\n area : " << area << "\n circumference : " << circumference << endl;
+    area = pi*r*r;
+    circumference = 2*pi*r;
 
   } else if(opt == 2){
     float s;
@@ -29,23 +29,40 @@ int main(){
     cout << "\n=== SQUARE ===\n side : ";
     cin >> s;
 
-    float area = s*s;
-    float circumference = 4*s;
-
-    cout << "\n=== Result ===\n area : " << area << "\n circumference : " << circumference << endl;
+    area = s*s;
+    circumference = 4*s;
 
   } else if(opt == 3){
-    float b,h;
+    float b,h,a,c;
 
     cout << "\n=== TRIANGLE ===\n base : ";
     cin >> b;
     cout << " height : ";
     cin >> h;
+    cout << " side A : ";
+    cin >> a;
+    cout << " side C : ";
+    cin >> c;
 
-    float area = (b*h)/2;
+    area = (b*h)/2;
+    circumference = a+b+c;
 
-    cout << "\n=== Result ===\n area : " << area << endl;
+  } else if(opt == 4){
+    float w,l;
+
+    cout << "\n=== RECTANGLE ===\n width : ";
+    cin >> w;
+    cout << " length : ";
+    cin >> l;
+
+    area = w*l;
+    circumference = 2*(w+l);
+
+  } else {
+    cout << "\nlooks like you choose the wrong option :((\n ";
   }
+
+  cout << "\n=== Result ===\n area : " << area << "\n circumference : " << circumference << endl;
 
   return 0;
 }
