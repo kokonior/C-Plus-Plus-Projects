c4b28289032d88ac1fdddc763a3439d2a47882e2
Nipun Garg <77457267+ngarg2k2@users.noreply.github.com>
2021-10-19 16:26:02 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 int main(){
-	int numberDigits;
-	cout << "How Many Digits Number Would you want to check : ";
-	cin >> numberDigits; cout << endl;
-	char number[numberDigits], number2[numberDigits];
-	cout << "Input your number : ";
-	for(int i=1; i<=numberDigits; i++){
-		cin >> number[i];
-	} cout << endl;
-	
-	// change value inverse
-	for(int i=1; i<=numberDigits; i++){
-		number2[i] = number[numberDigits+1-i];
-	}
-
-	// Check number value
-	for(int i=1; i<=numberDigits; i++){
-		if(number[i] != number2[i]) { cout << "This number is not palindrome"; return 0;}
-	}
-	cout << "This number palindrome"; return 1; 
+    int n, r, sum = 0, temp;
+    cout << "Enter the Number=";
+    cin >> n;
+    temp = n;
+    while (n > 0)
+    {
+        r = n % 10;
+        sum = (sum * 10) + r;
+        n = n / 10;
+    }
+    if (temp == sum)
+        cout << "Number is Palindrome.";
+    else
+        cout << "Number is not Palindrome.";
+    return 0;
 }
